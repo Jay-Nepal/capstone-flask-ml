a4baa020dfeaaf4eb04baeec2153d10a6192842b
JayNepal <lettertoaarogya@gmail.com>
2023-12-03 15:32:04 +0800

    Implemented employee dashboard

--- mainpage.php
+++ mainpage.php
@@ -4,22 +4,23 @@ include("include/mysql.php");
 
 $db = new myConnection;
 
-$home = $data_entry = "";
+$home = $claim_expense = "";
 
-if(isset($_GET['page'])=="home"){
+if(isset($_GET['module'])=="Claim_Expense"){
+  $claim_expense = "active";
+} else if(isset($_GET['module'])=="dashboard"){
   $home = "active";
-} else if(isset($_GET['page'])=="data_entry"){
-  $data_entry = "active";
 }
 
 ?>
+
 <!doctype html>
 <html lang="en">
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <title>LightXpense</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script
@@ -30,7 +31,7 @@ if(isset($_GET['page'])=="home"){
     <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
     <link rel="stylesheet" href="css/dashboard.css">
-    
+    <link rel="icon" type="image/png" href="images/logo2.png"/>     
 
 
     <style>
@@ -56,7 +57,7 @@ if(isset($_GET['page'])=="home"){
   <body>
 
   <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Company name</a>
+  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Welcome Dr. Narishah!</a>
   <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -74,27 +75,15 @@ if(isset($_GET['page'])=="home"){
       <div class="position-sticky pt-3">
         <ul class="nav flex-column">
         <li class="nav-item">
-            <a class="nav-link <?php echo $home;?>" aria-current="page" href="mainpage.php?module=dashboard&page=home">
+            <a class="nav-link" aria-current="page" href="mainpage.php?module=dashboard&page=home">
               <span data-feather="home"></span>
               Dashboard
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link <?php echo $data_entry;?>" href="mainpage.php?module=form&page=data_entry">
+            <a class="nav-link" href="mainpage.php?module=Claim%20Expense&page=expense">
               <span data-feather="file"></span>
-              Data Entry
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="mainpage.php?module=form&page=data_entry_table" class="nav-link">
-              <span data-feather="file"></span>
-              Data Entry Table
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="mainpage.php?module=form&page=data_chart" class="nav-link">
-              <span data-feather="file"></span>
-              Chart Data
+              Claim Expense
             </a>
           </li>
         </ul>
@@ -106,25 +95,12 @@ if(isset($_GET['page'])=="home"){
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
       <h1 class="h2"><?php echo ucfirst ($_GET['module']);?></h1>
-      
-        
-        <!-- <div class="btn-toolbar mb-2 mb-md-0">
-          <div class="btn-group me-2">
-            <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
-            <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
-          </div>
-          <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
-            <span data-feather="calendar"></span>
-            This week
-          </button>
-        </div> -->
       </div>
 
     <?php
         $module = $_GET['module'];
         $page = $_GET['page'];
         if(file_exists("module/".$module."/".$page.".php")){
-            // echo "ada";
             include("module/$module/$page.php");
         } else {
             echo "Error 404";
---
+++ module/Claim_Expense/claim_expense.php
@@ -0,0 +1,339 @@
+<?php
+
+$identry = isset($_GET['id']);
+
+if(isset($_POST['post'])==1){
+    $category = $_POST['slxCategory'];
+    $description = $_POST['description'];
+    $quantity = $_POST['quantity'];
+    $price = $_POST['price'];
+    $currDate = date("Y-m-d H:i:s");
+    $identry = $_POST['identry'];
+
+    if($identry > 0){
+        $sql = "UPDATE data_entry SET category='$category', `description`='$description', quantity='$quantity', price='$price' WHERE = id='$identry'";
+    } else {
+        // insert into tbl_entry
+        $sql = "INSERT INTO data_entry (category, `description`, quantity, price, created_date) VALUES ('$category', '$description','$quantity', '$price', '$currDate')";
+    }
+
+    $db->query($sql);
+}
+
+?>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+<style>
+
+* {
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+  font-family: "Poppins", sans-serif;
+}
+
+:root {
+  --dark-green: #006400;
+  --light-green: #20b2aa;
+  --light-grey: #f2f2f2;
+}
+
+html {
+  scroll-behavior: smooth;
+}
+
+body {
+  background-image: url("image/Web.png");
+  background-size: cover;
+}
+
+html,
+body {
+  max-width: 100%;
+  overflow-x: hidden;
+}
+
+.container {
+  padding-left: 15px;
+  padding-right: 15px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+  /* start claim status bar*/
+.status-bar {
+  display: flex;
+  align-items: center;
+  margin: 50px auto;
+  width: 45%;
+  justify-content: space-between;
+}
+
+.dot-green,
+.dot-grey {
+  position: relative;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  margin: 0 5px;
+}
+
+.dot-green {
+  background: var(--light-green);
+}
+
+.dot-grey {
+  background-color: #ccc;
+}
+
+.line-grey,
+.line-green {
+  height: 2px;
+  background-color: #ccc;
+  width: 200px;
+  margin: 0 5px;
+}
+
+.line-green {
+  background: var(--light-green);
+}
+
+.dot-label {
+  position: absolute;
+  top: 30px;
+  left: 50%;
+  transform: translateX(-50%);
+  font-size: 11px;
+  color: #333;
+}
+/* end claim status bar*/
+
+/* start claim */
+.claim .container {
+  max-width: 400px;
+  width: 100%;
+  background-color: #ededed;
+  padding: 30px;
+  border-radius: 30px;
+  margin-top: 10vh;
+}
+
+.img-area {
+  position: relative;
+  width: 100%;
+  height: 240px;
+  background-color: rgb(209, 207, 207);
+  margin-bottom: 30px;
+  border-radius: 15px;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+.img-area i {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 200px;
+  color: rgba(255, 255, 255, 0.5);
+  z-index: 1;
+}
+
+.img-area img {
+  width: 100%;
+  height: 100%;
+  object-fit: scale-down;
+}
+
+.img-area h3 {
+  font-size: 20px;
+  font-weight: 500;
+  margin-bottom: 6px;
+  z-index: 2;
+}
+
+.img-area p {
+  color: #999;
+  z-index: 2;
+}
+
+.img-area p span {
+  font-weight: 600;
+  z-index: 2;
+}
+
+.select-image {
+  display: block;
+  width: 100%;
+  padding: 16px 0;
+  border-radius: 15px;
+  background: var(--light-green);
+  color: #fff;
+  font-weight: 500;
+  font-size: 16px;
+  border: none;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  margin-bottom: 10px;
+  text-align: center;
+  text-decoration: none;
+}
+
+.select-image:hover {
+  background: var(--dark-green);
+}
+
+.submit {
+  display: block;
+  width: 100%;
+  padding: 16px 0;
+  border-radius: 15px;
+  background: var(--light-green);
+  color: #fff;
+  font-weight: 500;
+  font-size: 16px;
+  border: none;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  text-align: center;
+  text-decoration: none;
+}
+
+.submit:hover {
+  background: var(--dark-green);
+}
+/* end claim */
+
+/* start confirmation */
+.confirmation {
+  display: flex;
+}
+
+.image-container {
+  width: 50%;
+  text-align: center;
+  position: relative;
+  margin: 25px;
+}
+
+.image {
+  position: relative;
+  width: 80%;
+  height: 75vh;
+  background: var(--light-grey);
+  border-radius: 15px;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin: 0 auto;
+}
+
+.details-container {
+  width: 50%;
+  margin: 25px;
+}
+
+.details {
+  margin: 10px;
+}
+
+.details h6 {
+  padding-bottom: 20px;
+}
+
+.details-row {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+}
+
+.details-label {
+  font-weight: bold;
+  margin-bottom: 20px;
+  font-size: 15px;
+}
+
+.details-input {
+  padding: 10px;
+  border: 1px solid #ccc;
+  width: 65%;
+  height: 45px;
+  background: var(--light-grey);
+  color: #333;
+}
+
+.buttons {
+  display: flex;
+  justify-content: left;
+  align-items: center;
+  margin-top: 40px;
+}
+
+.button-process,
+.button-upload {
+  width: 30%;
+  margin-right: 35px;
+  padding: 15px 0;
+  border-radius: 15px;
+  font-weight: 500;
+  font-size: 16px;
+  border: none;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  text-align: center;
+  text-decoration: none;
+  background: var(--light-green);
+  color: #fff;
+}
+
+.button-process:hover,
+.button-upload:hover {
+  background: var(--dark-green);
+}
+
+/* end confirmation*/
+
+</style>
+
+<!--Start Status Bar-->
+<div class="status-bar">
+	    <div class="dot-green">
+		    <div class="dot-label">Claim</div>
+	    </div>
+	    <div class="line-grey"></div>
+	    <div class="dot-grey">
+		  <div class="dot-label">Confirmation</div>
+	    </div>
+	    <div class="line-grey"></div>
+	    <div class="dot-grey">
+		    <div class="dot-label">Dashboard</div>
+	    </div>
+    </div>
+    <!--End Status Bar-->
+     
+  <!-- Start Claim Expenses -->
+  <form action="">
+    <div class="claim">
+      <div class="container">
+        <input type="file" id="file" accept="image/*" hidden>
+        <div class="img-area">
+          <h3>Upload Your Receipt</h3>
+          <p>The file should be in <span>.png, .jpg, .jpeg, pdf</span></p>
+          <i class="fas fa-solid fa-cloud-arrow-up"></i>
+        </div>
+        <div class="claim-buttons">
+          <label for="file" class="select-image">Select Image</label>
+          <a href="confirmation.php" class="submit">Submit</a>
+        </div>
+      </div>
+    </div>
+  </form>
+  <!-- End Claim Expenses -->
+  
+  <script type="text/javascript" src="js/image_upload.js"></script>
